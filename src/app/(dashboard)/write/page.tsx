df109ebfd3a42b19f3726964cb094827ea642a33
Mohammed Abdul Jawad Vajeeh <jawadvajeeh@gmail.com>
2025-10-17 16:32:25 +0300

    wip: write page

--- src/app/(dashboard)/write/page.tsx
+++ src/app/(dashboard)/write/page.tsx
@@ -1,44 +1,66 @@
+'use client';
+
+import { ToggleGroupItem, ToggleGroupRoot } from '@/components/ui/toggle-group';
 import React from 'react';
 
+const categories = [
+  {
+    value: 'progress',
+    label: 'Progress',
+  },
+  {
+    value: 'challenge',
+    label: 'Challenge',
+  },
+  {
+    value: 'realization',
+    label: 'Realization',
+  },
+  {
+    value: 'reflection',
+    label: 'Reflection',
+  },
+  {
+    value: 'codeSnippet',
+    label: 'Code Snippet',
+  },
+  {
+    value: 'growthOrAchivement',
+    label: 'Growth/Achievement',
+  },
+];
+
 function Write() {
+  const [category, setCategory] = React.useState<string | null>(null);
+
   return (
     <div className="h-screen">
-      <div className="flex flex-col items-center gap-8 px-2 py-24">
+      <div className="flex flex-col items-center gap-12 px-2 py-24">
         <div className="flex flex-col items-center gap-4">
-          <p>TUESDAY, OCTOBER 14, 2025</p>
+          <h3 className="text-lg font-semibold">WEDNESDAY, OCTOBER 14, 2025</h3>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-5xl">Good Morning.</p>
-            <p>Take a moment to reflect on your journey</p>
+            <h1 className="text-center text-6xl font-bold">Good Morning.</h1>
+            <p className="text-xl">Take a moment to reflect on your journey.</p>
           </div>
         </div>
-        <div className="flex w-full max-w-4xl flex-col gap-6 rounded-md bg-white p-8">
+        <div className="flex w-full flex-col rounded-md bg-white p-8">
           <div>
-            <p>Category</p>
-            <div className="mt-2 flex flex-wrap gap-2">
-              <button className="bg-bgMain rounded-full px-4 py-1">Progress</button>
-              <button className="bg-bgMain rounded-full px-4 py-1">Challenge</button>
-              <button className="bg-bgMain rounded-full px-4 py-1">Realization</button>
-              <button className="bg-bgMain rounded-full px-4 py-1">Reflection</button>
-              <button className="bg-bgMain rounded-full px-4 py-1">Code Snippet</button>
-              <button className="bg-bgMain rounded-full px-4 py-1">Growth/Achievement</button>
-            </div>
-          </div>
-          <div>
-            <p>Title</p>
-            <input
-              className="focus:outline-bg100 bg-bgMain w-full rounded-md p-2 outline-none focus:outline-1 focus:outline-solid"
-              type="text"
-              placeholder="Give your entry a meaningful title ..."
-            />
-          </div>
-          <div>
-            <p>Your Thoughts</p>
-            <textarea
-              placeholder="Pour your thoughts into this page..."
-              className="focus:outline-bg100 bg-bgMain w-full resize-none rounded-md p-2 focus:outline-solid"
-              name=""
-              id=""
-            />
+            <h4 className="mb-2 font-semibold">Category</h4>
+            <ToggleGroupRoot
+              className="flex flex-wrap gap-2"
+              value={category}
+              onValueChange={setCategory}
+            >
+              {categories.map((cat) => (
+                <ToggleGroupItem
+                  className="border-border100 data-[state=on]:bg-nuetral100 data-[state=on]:text-border100 rounded-full border px-2 py-1 md:px-4"
+                  key={cat.value}
+                  value={cat.value}
+                >
+                  {cat.label}
+                </ToggleGroupItem>
+              ))}
+            </ToggleGroupRoot>
           </div>
         </div>
       </div>
---
+++ src/components/ui/toggle-group.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import * as React from 'react';
+
+type ToggleGroupRootCtxt = {
+  value: string | null;
+  onValueChange?: (value: string | null) => void;
+};
+
+const ToggleGroupRootCtxt = React.createContext<ToggleGroupRootCtxt | undefined>(undefined);
+
+const useToggleGroup = () => {
+  const context = React.useContext(ToggleGroupRootCtxt);
+  if (!context) {
+    throw new Error('useToggleGroup must be used within a ToggleGroupRoot');
+  }
+  return context;
+};
+
+type ToggleGroupRootProps = {
+  children: React.ReactNode;
+  value: string | null;
+  onValueChange?: (value: string | null) => void;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export function ToggleGroupRoot({
+  children,
+  value,
+  onValueChange = () => {},
+  ...props
+}: ToggleGroupRootProps) {
+  return (
+    <ToggleGroupRootCtxt.Provider value={{ value, onValueChange }}>
+      <div {...props}>{children}</div>
+    </ToggleGroupRootCtxt.Provider>
+  );
+}
+
+type ToggleGroupItemProps = {
+  children: React.ReactNode;
+  value: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function ToggleGroupItem({ children, value, ...props }: ToggleGroupItemProps) {
+  const ctxt = useToggleGroup();
+
+  const isToggleOn = ctxt.value === value;
+
+  function handleToggleItem() {
+    if (ctxt.onValueChange) {
+      ctxt.onValueChange(isToggleOn ? null : value);
+    }
+  }
+
+  return (
+    <button onClick={handleToggleItem} {...props} data-state={isToggleOn ? 'on' : 'off'}>
+      {children}
+    </button>
+  );
+}
