8166b48665efe4789b61e27c2f62daf65865932c
Mohammed Abdul Jawad Vajeeh <jawadvajeeh@gmail.com>
2025-11-01 16:51:29 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className="dark1" lang="en">
+    <html lang="en">
       <body className={`${montserrat.variable} antialiased`}>{children}</body>
     </html>
   );
